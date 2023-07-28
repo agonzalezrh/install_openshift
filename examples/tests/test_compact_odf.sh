81e1354e98056d0f027f946dd28651a609ed34bc
Alberto Gonzalez <josegonz@redhat.com>
2023-07-28 17:35:47 +0300

    New adding vSphere examples

---
+++ examples/tests/test_compact_odf.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+TESTNAME="compact-odf"
+cd ../
+
+# Delete existing environments
+R_CLUSTER_IDS=$(ANSIBLE_STDOUT_CALLBACK=json ansible-playbook get_cluster_id_by_name.yaml -e cluster_name=${TESTNAME} 2>/dev/null \
+|  jq -r '.plays[0].tasks[-1].hosts.localhost.filter_ids')
+echo $R_CLUSTER_IDS
+if [ "${R_CLUSTER_IDS}" != "VARIABLE IS NOT DEFINED!" ]; then
+  CLUSTER_IDS=$(echo $R_CLUSTER_IDS | jq -r '.[]')
+  for cluster_id in ${CLUSTER_IDS}; do
+    ansible-playbook compact_odf_kubevirt.yaml -e action='delete' -e cluster_id=${cluster_id} -e cluster_name=${TESTNAME} -e ocp_namespace=test-${TESTNAME}
+  done
+fi
+if oc get project test-${TESTNAME}; then
+  oc delete project test-${TESTNAME} --wait=true
+fi
+# Create new environment
+
+oc new-project test-${TESTNAME} 
+ansible-playbook compact_odf_kubevirt.yaml -e cluster_name=${TESTNAME} -e ocp_namespace=test-${TESTNAME} -e overwrite_dns=true
+
